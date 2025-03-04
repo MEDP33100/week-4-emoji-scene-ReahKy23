da0dc64fbfb328de35463848eaff17e019fe69f6
ReahKy23 <Kylah.kerr23@gmail.com>
2025-03-03 19:49:57 -0500

    movement
    Both emojis moving in the way I want. Now have to use timelines to have emoji change direction.

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    // gsap code here!
+
+
+    gsap.fromTo(".left-emojis", 
+        { x: 0 }, // Start from 500px to the right
+        { x: 400, duration: 5, ease: "power1.inOut" } // Move left
+    );
+
+    gsap.fromTo(".boy", 
+        { x: 100 }, // Start from 500px to the right
+        { x: -100, duration: 5, ease: "power1.inOut" } // Move left
+    );
+
+    gsap.from('.ball', {
+        y: -60,
+        duration: 0.6,
+        repeat: -1,
+
+    })
+   });
